a9395d8874e8bdf1fc00ccf88d486a76d4b7bb62
Max Dvoynishnikov <maxim.dvoynishnikov@tccenter.ru>
2020-04-09 23:38:41 +0300

    dd

--- packages/app/index.js
+++ packages/app/index.js
@@ -1,6 +1,6 @@
 const first = require('@my-super-first-monorepo/first');
 
-const app = () => 'Hi from the app!';
+const app = () => 'Hi from the app!!!!';
 
 const main = () => {
   console.log(app());
